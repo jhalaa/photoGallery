57b75edb9329e69a421a6ef53117d733c7e7e246
Jhalaa <jhalaachinoy@gmail.com>
2019-07-23 23:12:27 -0700

    Adding carousel from reactstrap

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,13 +1,66 @@
 import React, {Component} from 'react';
-
+import {Carousel, CarouselItem, CarouselControl, CarouselIndicators, CarouselCaption} from 'reactstrap';
 
 class Gallery extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {activeIndex: 0};
+        this.next = this.next.bind(this);
+        this.previous = this.previous.bind(this);
+        this.goToIndex = this.goToIndex.bind(this);
+        this.onExiting = this.onExiting.bind(this);
+        this.onExited = this.onExited.bind(this);
+    }
+
+    onExiting() {
+        this.animating = true;
+    }
+
+    onExited() {
+        this.animating = false;
+    }
+
+    next() {
+        if (this.animating) return;
+        const nextIndex = this.state.activeIndex === this.props.images.length - 1 ? 0 : this.state.activeIndex + 1;
+        this.setState({activeIndex: nextIndex});
+    }
+
+    previous() {
+        if (this.animating) return;
+        const nextIndex = this.state.activeIndex === 0 ? this.props.images.length - 1 : this.state.activeIndex - 1;
+        this.setState({activeIndex: nextIndex});
+    }
+
+    goToIndex(newIndex) {
+        if (this.animating) return;
+        this.setState({activeIndex: newIndex});
+    }
+
     render() {
-        let imageList = this.props.images.map(image => <img src={image.url}/>);
+        let imageList = this.props.images.map((image) => {
+            return (
+                <CarouselItem
+                    onExiting={this.onExiting}
+                    onExited={this.onExited}
+                    key={image.url}>
+                    <img src={image.url} alt={image.caption}/>
+                    <CarouselCaption captionText={image.caption} captionHeader={image.caption}/>
+                </CarouselItem>
+            )
+        });
         return (
-            <div>
+            <Carousel
+                activeIndex={this.state.activeIndex}
+                next={this.next}
+                previous={this.previous}
+            >
+                <CarouselIndicators items={this.props.images} activeIndex={this.state.activeIndex}
+                                    onClickHandler={this.goToIndex}/>
                 {imageList}
-            </div>
+                <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous}/>
+                <CarouselControl direction="next" directionText="Next" onClickHandler={this.next}/>
+            </Carousel>
         );
     }
 

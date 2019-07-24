c6596403004675a8df8ca30a4af207779fc4f53e
Jhalaa <jhalaachinoy@gmail.com>
2019-07-24 10:19:18 -0700

    small style change

--- src/CustomIndicator.js
+++ src/CustomIndicator.js
@@ -10,7 +10,7 @@ class CustomIndicator extends React.Component {
                     this.props.items.map((elem, index) =>
                         <img
                             className={this.props.activeIndex == index ? 'custom-indicator-active' : ''}
-                            key={index} onClick={() => this.props.handleClick(index)} height="150px" src={elem.url}
+                            key={index} onClick={() => this.props.handleClick(index)} height="120px" src={elem.url}
                             alt={elem.caption}/>
                     )}
             </div>

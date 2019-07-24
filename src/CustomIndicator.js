7f079a09100e463290ae6ce6f92d5cbe8b73e01b
Jhalaa <jhalaachinoy@gmail.com>
2019-07-24 09:38:30 -0700

    css changes

--- src/CustomIndicator.js
+++ src/CustomIndicator.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import './CustomIndicator.css'
 
 class CustomIndicator extends React.Component {
     render() {
         return (
             this.props.items.map((elem, index) =>
-                <img key={index} onClick={() => this.props.handleClick(index)} height="150px" src={elem.url}
+                <img className={this.props.activeIndex==index?'custom-indicator-active' : 'custom-indicator-inactive'} key={index} onClick={() => this.props.handleClick(index)} height="150px" src={elem.url}
                      alt={elem.caption}/>
             )
         );

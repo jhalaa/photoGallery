2f92d26ea17dfeb47af5fdf3c389dd85d19f37bd
Jhalaa <jhalaachinoy@gmail.com>
2019-07-23 22:20:09 -0700

    Displaying the images inside gallery component

--- src/Gallery.js
+++ src/Gallery.js
@@ -3,9 +3,10 @@ import React, {Component} from 'react';
 
 class Gallery extends Component {
     render() {
+        let imageList = this.props.images.map(image => <img src={image.url}/>);
         return (
             <div>
-
+                {imageList}
             </div>
         );
     }

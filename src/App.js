b7f557d9b20338b78c360b38f033b739b0b6c48d
Jhalaa <jhalaachinoy@gmail.com>
2019-07-23 22:10:16 -0700

    Passing an array of url and caption as images

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import './App.css';
 import Gallery from './Gallery'
 
 function App() {
-  var images = ["Image1", "Image2","Image3","Image4"];
+  const images = [{url:"https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png",caption:"Bulbasaur"},
+    {url:"https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png",caption:"Charmander"},
+    {url:"https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png",caption:"Squirtle"},
+    {url:"https://assets.pokemon.com/assets/cms2/img/pokedex/detail/050.png",caption:"Diglet"},
+    {url:"https://assets.pokemon.com/assets/cms2/img/pokedex/detail/012.png",caption:"Butterfree"},
+    {url:"https://assets.pokemon.com/assets/cms2/img/pokedex/detail/039.png",caption:"Jigglypuff"},
+    {url:"https://assets.pokemon.com/assets/cms2/img/pokedex/detail/093.png",caption:"Haunter"},
+    {url:"https://assets.pokemon.com/assets/cms2/img/pokedex/detail/120.png",caption:"Staryu"},
+    {url:"https://assets.pokemon.com/assets/cms2/img/pokedex/detail/088.png",caption:"Grimer"}];
   return (
     <div className="App">
       <Gallery images={images}/>
